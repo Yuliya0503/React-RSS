688c434c637e5de852e02b64c3078e564fd86c23
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-03 20:46:44 +0100

    feat: change card component from class component to functional component

--- src/components/CardPeople.tsx
+++ src/components/CardPeople.tsx
@@ -1,63 +1,47 @@
-import React from 'react';
-import { ICardPeopleProps, ICardPeopleState } from '../models/types';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { ICardPeopleProps } from '../models/types';
 import styles from './cardPeople.module.css';
 import PlanetService from '../API/PlanetService';
-import { IPlanet } from '../models/ISWAPI';
 
-export default class CardPeople extends React.Component<
-  ICardPeopleProps,
-  ICardPeopleState
-> {
-  constructor(props: ICardPeopleProps) {
-    super(props);
-    this.state = {
-      planet: {
-        name: '',
-      },
-      loading: true,
-      error: false,
-    };
-  }
+const CardPeople: React.FC<ICardPeopleProps> = ({ person }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [planetName, setPlanetName] = useState('');
+  const abortControllerRef = useRef(new AbortController());
 
-  setPlanet = async (url: string): Promise<void> => {
+  const setPlanet = useCallback(async (url: string): Promise<void> => {
     try {
-      const planetInfo: IPlanet = await PlanetService.getPlanet(url);
-      this.setState({
-        planet: planetInfo,
-        loading: false,
-      });
+      const planetInfo = await PlanetService.getPlanet(url);
+      setPlanetName(planetInfo.name);
+      setLoading(false);
     } catch (error: Error | unknown) {
       console.error('Oops!', error);
-      this.setState({ loading: false, error: true });
+      setLoading(false);
+      setError(true);
     }
-  };
+  }, []);
 
-  componentDidMount = (): void => {
-    this.setPlanet(this.props.person.homeworld);
-  };
+  useEffect(() => {
+    const abortController = abortControllerRef.current;
+    setPlanet(person.homeworld);
+    return () => {
+      abortController.abort();
+    };
+  }, [person.homeworld, setPlanet]);
 
-  render = (): JSX.Element => {
-    const { person } = this.props;
-    const planet: IPlanet = this.state.planet;
-    return (
-      <div className={styles.people_card}>
-        <h2>{person.name}</h2>
-        <p>Gender: {person.gender}</p>
-        <p>Birth Year: {person.birth_year}</p>
-        <p>Eye Color: {person.eye_color}</p>
-        <p>Hair Color: {person.hair_color}</p>
-        <p>Skin Color: {person.skin_color}</p>
-        <p>Height: {person.height} cm</p>
-        <p>Mass: {person.mass} kg</p>
-        <p>
-          Home World:{' '}
-          {this.state.loading
-            ? 'Loading'
-            : this.state.error
-            ? 'Oops!'
-            : planet.name}
-        </p>
-      </div>
-    );
-  };
-}
+  return (
+    <div className={styles.people_card}>
+      <h2>{person.name}</h2>
+      <p>Gender: {person.gender}</p>
+      <p>Birth Year: {person.birth_year}</p>
+      <p>Eye Color: {person.eye_color}</p>
+      <p>Hair Color: {person.hair_color}</p>
+      <p>Skin Color: {person.skin_color}</p>
+      <p>Height: {person.height} cm</p>
+      <p>Mass: {person.mass} kg</p>
+      <p>Home World: {loading ? 'Loading' : error ? 'Oops!' : planetName}</p>
+    </div>
+  );
+};
+
+export default CardPeople;
