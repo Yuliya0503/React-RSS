7b6beba576496ad269c23c5e34be742d3e4d69f1
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-03 22:11:56 +0100

    feat: change SearchResult component from class component to functional component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -3,13 +3,18 @@ import SearchInput from './SearchInput';
 import { ISearchInputProps } from '../models/types';
 import styles from './SearchResults.module.css';
 
-export default class SearchResult extends React.Component<ISearchInputProps> {
-  render = (): JSX.Element => {
-    return (
-      <header className={styles.header}>
-        <h1 className={styles.title}>Star Wars</h1>
-        <SearchInput {...this.props} />
-      </header>
-    );
-  };
-}
+const SearchResult: React.FC<ISearchInputProps> = (props) => {
+  const { updateCards, setLoading, setError } = props;
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Star Wars</h1>
+      <SearchInput
+        updateCards={updateCards}
+        setError={setError}
+        setLoading={setLoading}
+      />
+    </header>
+  );
+};
+
+export default SearchResult;
