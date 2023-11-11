e5233abea93597a4a3ddb8376cfbd0ce82da1247
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-11 15:12:45 +0100

    fix: fix errors in the component searchPage

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -1,50 +1,68 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { ISearchPageProps } from '../../models/types';
-import styles from './SearchPage.module.css';
-import ErrorSection from '../Error/ErrorSection';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Pagination } from '../Pagination/Pagination';
 import PeopleSection from '../PeopleSection/PeopleSection';
-import NoResultSection from '../NoResultSection/NoResultSection';
-import { IResponse } from '../../models/ISWAPI';
-import getPeople from '../../API/CardService';
+import Header from '../Header/Header';
+import { usePeope } from '../../hooks/usePeople';
+import {
+  DEFAULT_PAGE,
+  DEFAULT_LIMIT,
+  SEARCH_PARAM_PAGE,
+} from '../../models/constants';
 
-const SearchPage: React.FC<ISearchPageProps> = ({
-  cards,
-  error,
-  countPages,
-  setCards,
-  setLoading,
-}) => {
-  const navigate = useNavigate();
-  const paginationButton = Array.from({ length: countPages }, (_, i) => (
-    <button key={i + 1} onClick={() => handlePageClick(i + 1)}>
-      {i + 1}
-    </button>
-  ));
+interface SearchPageProps {}
 
-  async function handlePageClick(pageNumber: number) {
-    console.log('SearchPahe:', pageNumber);
-    navigate(`/page/${pageNumber}`);
-    localStorage.setItem('PageNumber', pageNumber.toString());
-    setLoading(true);
-    const { results }: IResponse = await getPeople(pageNumber);
-    setCards(results);
-    setLoading(false);
-  }
+const SearchPage: React.FC<SearchPageProps> = () => {
+  const [searchTerm, setSearchTerm] = useState<string>(
+    localStorage.getItem('searchTerm') || ''
+  );
+  const [currentPage, setCurrentPage] = useState<number>(DEFAULT_PAGE);
+  const [limit, setLimit] = useState<number>(DEFAULT_LIMIT);
+  const [data, isLoading, totalResults] = usePeope(
+    searchTerm,
+    currentPage,
+    limit
+  );
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setSearchTerm(localStorage.getItem('searchTerm') || '');
+  }, []);
+
+  const handleSearchClick = (search: string) => {
+    setSearchTerm(search);
+    initFirstPage();
+  };
+
+  const handleLimitChange = (newLimit: number) => {
+    setLimit(newLimit);
+    initFirstPage();
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+    setSearchParams({ [SEARCH_PARAM_PAGE]: newPage.toString() });
+  };
+
+  const initFirstPage = () => {
+    setCurrentPage(DEFAULT_PAGE);
+    searchParams.delete(SEARCH_PARAM_PAGE);
+    setSearchParams(searchParams);
+  };
 
   return (
-    <>
-      <div className={styles.wrapper}>
-        {error ? (
-          <ErrorSection />
-        ) : cards.length ? (
-          <PeopleSection cards={cards} />
-        ) : (
-          <NoResultSection />
-        )}
-      </div>
-      <div>{paginationButton}</div>
-    </>
+    <main>
+      <Header onClick={handleSearchClick} searchTerm={searchTerm} />
+      <PeopleSection isLoading={isLoading} data={data} limit={limit}>
+        <Pagination
+          currentPage={currentPage}
+          totalItems={totalResults}
+          itemsPerPage={limit}
+          onPageChange={handlePageChange}
+          onItemsPerPageChange={handleLimitChange}
+        />
+      </PeopleSection>
+    </main>
   );
 };
 
--- src/models/constants.ts
+++ src/models/constants.ts
@@ -1,3 +1,6 @@
 export const ErrorMessage = 'Something went wrong, please reload the page';
 export const NoResultMessage = 'No result found.';
-export const defaultSearch = '?page=1';
+
+export const DEFAULT_PAGE = 1;
+export const DEFAULT_LIMIT = 10;
+export const SEARCH_PARAM_PAGE = 'page';
