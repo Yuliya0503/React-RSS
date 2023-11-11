648d2a3df9f147c30f280a632532d3f00b6ef8dd
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-11 12:36:42 +0100

    fix: fix errors in the component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,58 +1,29 @@
 import React, { useState } from 'react';
-import { IResponse } from '../../models/ISWAPI';
-import { ISearchInputProps } from '../../models/types';
 import styles from './SearchInput.module.css';
-import getPeople from '../../API/CardService';
-import SearchField from '../SearchField/SearchField';
-import SearchButton from '../SearchButton/SearchButton';
-import { useNavigate } from 'react-router-dom';
+import SearchField from './SearchField/SearchField';
+import SearchButton from './SearchButton/SearchButton';
 
-const SearchInput: React.FC<ISearchInputProps> = ({
-  updateCards,
-  setLoading,
-  setError,
-}) => {
-  const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState(
-    localStorage.getItem('searchTerm') || ''
-  );
+interface SearchInputProps {
+  onClick: (searchTerm: string) => void;
+  searchTerm: string;
+}
 
-  const handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ): void => {
-    setSearchTerm(event.target.value.trim());
+const SearchInput: React.FC<SearchInputProps> = ({ onClick, searchTerm }) => {
+  const [inputValue, setInputValue] = useState(searchTerm);
+
+  const handleInputChange = (value: string) => {
+    setInputValue(value);
   };
 
-  const handleSearch = async (event: React.FormEvent): Promise<void> => {
-    event.preventDefault();
-    const searchQuery = searchTerm.trim();
-    const endPoint = searchQuery ? `?search=${searchQuery}` : '?page=1';
-    if (searchQuery) {
-      navigate(`/?search=${searchQuery}`);
-      console.log('EendPoint', searchQuery);
-
-      localStorage.setItem('lastSearch', searchTerm);
-      try {
-        setLoading(true);
-        const { results }: IResponse = await getPeople(endPoint);
-        updateCards(results);
-        setLoading(false);
-      } catch (error) {
-        console.error(`Error: ${error}`);
-        setLoading(false);
-        setError(true);
-      }
-    }
+  const handleSearchClick = () => {
+    onClick(inputValue.trim());
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSearch}>
-      <SearchField
-        searchTerm={searchTerm}
-        handleInputChange={handleInputChange}
-      />
-      <SearchButton />
-    </form>
+    <div className={styles.form}>
+      <SearchField value={inputValue} onChange={handleInputChange} />
+      <SearchButton onClick={handleSearchClick} />
+    </div>
   );
 };
 
