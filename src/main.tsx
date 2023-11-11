ee481dd3d91cc480ba1cdb5ea72ff4cd929aebc9
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-11 15:55:45 +0100

    feat: add router and change file main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-//import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { AppRouter } from './Router/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={AppRouter} />
 );
