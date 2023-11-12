ca50f9ded6b35b1b42c09a51abde406025dba2e0
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-12 16:12:23 +0100

    refactor: use constatns to handlers

--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -1,22 +1,23 @@
 import { http, HttpResponse } from 'msw';
 import { PeopleResponse } from '../data/peopleResponse';
+import ConstantsURL from '../../API/constants';
 
 export const handlers = [
-  http.get('https://swapi.dev/api/people', () => {
+  http.get(ConstantsURL.PEOPLE_URL, () => {
     return HttpResponse.json(PeopleResponse);
   }),
 
-  http.get('https://swapi.dev/api/people/1', () => {
+  http.get(`${ConstantsURL.PEOPLE_URL}1`, () => {
     return HttpResponse.json(PeopleResponse.results[0]);
   }),
 
-  http.get('https://swapi.dev/api/people/100', () => {
+  http.get(`${ConstantsURL.PEOPLE_URL}100`, () => {
     setTimeout(() => {
       return HttpResponse.json(PeopleResponse.results[0]);
     });
   }),
 
-  http.get('https://swapi.dev/api/people/NaN', () => {
+  http.get(`${ConstantsURL.PEOPLE_URL}NaN`, () => {
     return HttpResponse.error();
   }),
 ];
