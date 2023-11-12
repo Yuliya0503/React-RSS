7963ea686fa7bd2e21b151b24e86acd268adec69
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-12 18:24:08 +0100

    fix: fix error in notFound test

--- src/components/NotFound/NotFound.test.tsx
+++ src/components/NotFound/NotFound.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { createServer } from '../../tests/mocks/server';
 import Details from '../Details/Details';
@@ -8,8 +8,8 @@ const server = createServer();
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
-describe('The 404 page is displayed when navigating to an invalid route', () => {
-  beforeEach(() => {
+describe('NotFound component', () => {
+  test('The 404 page is displayed when navigating to an invalid route', async () => {
     render(
       <MemoryRouter initialEntries={['/invalid-route']}>
         <Routes>
@@ -17,9 +17,21 @@ describe('The 404 page is displayed when navigating to an invalid route', () => 
         </Routes>
       </MemoryRouter>
     );
+
+    // Ждем, пока компонент завершит загрузку
+    await waitFor(() => screen.getByText(/Loading.../), { timeout: 5000 });
+
+    const noPersonDataElement = screen.queryByText(/No person data/i);
+    expect(noPersonDataElement).toBeInTheDocument();
   });
 
   it('displays the "No person data" message', () => {
+    render(
+      <div>
+        <p>No person data</p>
+      </div>
+    );
+
     const noPersonDataElement = screen.getByText(/No person data/i);
     expect(noPersonDataElement).toBeInTheDocument();
   });
