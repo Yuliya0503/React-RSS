665a4dad044afa00a992f6e38cf9d3be981dbfee
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-12 12:28:53 +0100

    fix: fix error in useSearch hook

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -2,11 +2,7 @@ import { useContext } from 'react';
 import { SearchContext } from '../Context/SearchContext';
 
 export const useSearch = (): string => {
-  const search = useContext(SearchContext) as string | undefined;
-
-  if (!search) {
-    throw new Error('Search context is not provided');
-  }
+  const search = useContext(SearchContext) as string;
 
   return search;
 };
