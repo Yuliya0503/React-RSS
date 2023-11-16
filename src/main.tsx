8a2cd6bff6a0626f9f049e45475fd07cc53aa0dd
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-16 16:03:15 +0100

    feat: add file preparation for Redux Store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import './index.css';
 import { AppRouter } from './Router/router';
+import { store } from './Store/Store';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={AppRouter} />
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={AppRouter} />
+    </Provider>
+  </StrictMode>
 );
