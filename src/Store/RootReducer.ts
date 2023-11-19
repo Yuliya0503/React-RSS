431a9cecaef3c926698e466e4789d5ee140e4d4e
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-19 13:51:01 +0100

    fix: fix file name and fix path

--- src/Store/RootReducer.ts
+++ src/Store/RootReducer.ts
@@ -1,16 +1,18 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import searchSliceReducer from './Reducers/SearchReduser';
-import PeopleListReduser from './Reducers/PeopleListReduser';
 import PageSliceReduser from './Reducers/PageSliceReduser';
 import DetailSliceReduser from './Reducers/DetailsReduser';
 import loadingReduser from './Reducers/loadingReduser';
+import PageCurrentSlice from './Reducers/PageCurrentSlice';
+import { apiPeople } from '../API/CardService';
 
 export const rootReducer = combineReducers({
   searchSlice: searchSliceReducer,
-  peopleListSlice: PeopleListReduser,
   pageSlice: PageSliceReduser,
   detailsSlice: DetailSliceReduser,
   loadingSlice: loadingReduser,
+  pageCurrent: PageCurrentSlice,
+  [apiPeople.reducerPath]: apiPeople.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- src/hooks/reduxHoooks.ts
+++ src/hooks/reduxHoooks.ts
@@ -1,6 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '../Store/Store';
-import { RootState } from '../Store/RootReduser';
+import { RootState } from '../Store/RootReducer';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
