2b0fc8c257af28cdcd6526463343bc01d9b31f0d
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-19 17:42:21 +0100

    feat: update api function

--- src/API/CardService.ts
+++ src/API/CardService.ts
@@ -1,20 +1,26 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import ConstantsURL from './constants';
+import { IPeople, IResponse } from '../models/ISWAPI';
 const baseUrl = ConstantsURL.PEOPLE_URL;
 
 export const apiPeople = createApi({
-  reducerPath: 'peopleApi',
+  reducerPath: 'apiPeople',
+  tagTypes: ['People'],
   baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
   endpoints: (builder) => ({
-    getPeople: builder.query({
+    getPeople: builder.query<IResponse, string>({
       query: (searchParams: string) => ({
         url: searchParams ? `?${searchParams}` : '',
       }),
+      providesTags: ['People'],
     }),
-    getPerson: builder.query({
+    getPerson: builder.query<IPeople, string>({
       query: (id: string) => id,
     }),
   }),
 });
+
+export const selectPerson = (id: string) =>
+  apiPeople.endpoints.getPeople.select(id);
 
 export const { useGetPeopleQuery, useGetPersonQuery } = apiPeople;
