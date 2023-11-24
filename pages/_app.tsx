ce7ae85b80c7598345ea5227f0902a925574a7da
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-24 22:08:50 +0100

    feat: connect styles and add an error button to the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import RootLayout from './layout';
 import ErrorBoundary from '@/src/components/ErrorBoundary/ErrorBoundary';
+import ErrorButton from '@/src/components/Error/ErrorButton';
+import '../src/styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <RootLayout>
     <ErrorBoundary>
+      <ErrorButton />
       <Component {...pageProps} />
     </ErrorBoundary>
   </RootLayout>
