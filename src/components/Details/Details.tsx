256e4a9ab197ea1c74bd36cd929a39ff4783fb58
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-25 18:26:51 +0100

    feat: add use next.js route to details

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,35 +1,38 @@
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Loading from '../Loading/Loading';
-import NotFound from '../NotFound/NotFound';
-import NoResultSection from '../NoResultSection/NoResultSection';
-import { useGetPersonQuery } from '../../API/CardService';
-import useActions from '../../hooks/useActions';
+// import Loading from '../Loading/Loading';
+// import NotFound from '../NotFound/NotFound';
+// import NoResultSection from '../NoResultSection/NoResultSection';
+// import { useGetPersonQuery } from '../../API/CardService';
 import styles from './Details.module.css';
 import DetailsInfo from './DetailsInfo/DetailsInfo';
+import { encode } from 'querystring';
+import { DEFAULT_LIMIT, DEFAULT_PAGE } from '@/src/models/constants';
+import { IPeople } from '@/src/models/ISWAPI';
 
-const Details = (): JSX.Element => {
+interface DetailsProps {
+  person: IPeople;
+}
+
+const Details = ({ person }: DetailsProps): JSX.Element => {
   const router = useRouter();
-  const { id } = router.query as { id: string };
-  const { data, isFetching, isError } = useGetPersonQuery(id);
-  const { loadingdetails } = useActions();
-
-  useEffect(() => {
-    loadingdetails(isFetching);
-  }, [loadingdetails, isFetching]);
+  const searchParams = new URLSearchParams(encode(router.query));
+  const limit: number = Number(searchParams.get('limit')) || DEFAULT_LIMIT;
+  const currentPage: number = Number(searchParams.get('page')) || DEFAULT_PAGE;
+  const search: string = searchParams.get('search') || '';
+  const commonQuery = { pathname: '/', query: { search, currentPage, limit } };
 
   const handleClick = (): void => {
-    router.push('/');
+    router.push(commonQuery);
   };
 
   return (
     <>
-      {isFetching && <Loading />}
+      {/* {isFetching && <Loading />}
       {isError && <NotFound />}
-      {!isFetching && !data && <NoResultSection />}
-      {data && (
+      {!isFetching && !data && <NoResultSection />} */}
+      {person && (
         <div className={styles.wrapper}>
-          <DetailsInfo person={data} />
+          <DetailsInfo person={person} />
           <div className={styles.button_wrapper}>
             <button className={styles.button} onClick={handleClick}>
               Close
