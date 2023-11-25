38352cc710e1b647b4b3a6b967d9fc7374949ac4
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-25 17:07:00 +0100

    feat: remove redusers

--- src/Store/Reducers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as loadingReduser } from './loadingReduser';
-export { default as pageCurrentReduser } from './PageCurrentSlice';
-export { default as pageSliceReduser } from './PageSliceReduser';
-export { default as searchReduser } from './SearchReduser';
