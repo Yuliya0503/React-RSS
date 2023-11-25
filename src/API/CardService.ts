8c253240a231c32d475005c6ae7c69eed499303c
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-25 17:15:36 +0100

    feat: change card service

--- src/API/CardService.ts
+++ src/API/CardService.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import ConstantsURL from './constants';
 import { IPeople, IResponse } from '../models/ISWAPI';
+import { HYDRATE } from 'next-redux-wrapper';
 
 const baseUrl = ConstantsURL.PEOPLE_URL;
 const GET_PEOPLE_ENDPOINT = 'getPeople';
@@ -8,22 +9,28 @@ const GET_PERSON_ENDPOINT = 'getPerson';
 
 export const apiPeople = createApi({
   reducerPath: 'apiPeople',
-  tagTypes: ['People'],
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     [GET_PEOPLE_ENDPOINT]: builder.query<IResponse, string>({
       query: (searchParams: string) => ({
         url: searchParams ? `?${searchParams}` : '',
       }),
-      providesTags: ['People'],
     }),
     [GET_PERSON_ENDPOINT]: builder.query<IPeople, string>({
       query: (id: string) => id,
     }),
   }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    return action.type === HYDRATE ? action.payload[reducerPath] : undefined;
+  },
 });
 
-export const { useGetPeopleQuery, useGetPersonQuery } = apiPeople;
+export const {
+  useGetPeopleQuery,
+  useGetPersonQuery,
+  util: { getRunningQueriesThunk },
+} = apiPeople;
 
-export const selectPerson = (id: string) =>
-  apiPeople.endpoints[GET_PERSON_ENDPOINT].select(id);
+export const { select: selectPerson } = apiPeople.endpoints.getPerson;
+
+export const { getPeople, getPerson } = apiPeople.endpoints;
