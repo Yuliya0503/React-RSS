092f0e61ac1b066fe030afe840e8e1e4a48c5d17
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2023-11-26 18:03:17 +0100

    fix: fix errors in people section

--- src/components/PeopleSection/PeopleSection.tsx
+++ src/components/PeopleSection/PeopleSection.tsx
@@ -2,10 +2,8 @@ import { PropsWithChildren, useEffect, useState } from 'react';
 import { IPeople, IResponse } from '../../models/ISWAPI';
 import Card from '../Card/Card';
 import NoResultSection from '../NoResultSection/NoResultSection';
-//import Loading from '../Loading/Loading';
 import styles from './PeopleSection.module.css';
 import Pagination from '../Pagination/Pagination';
-//import NotFound from '../NotFound/NotFound';
 import { Router, useRouter } from 'next/router';
 import { encode } from 'querystring';
 import { DEFAULT_LIMIT } from '@/src/models/constants';
@@ -15,29 +13,8 @@ interface Props {
   people: IResponse;
 }
 
-// const buildSearchParams = (
-//   searchTerm: string,
-//   limit: number,
-//   currentPage: number
-// ): string => {
-//   const searchParams = new URLSearchParams();
-//   searchTerm && searchParams.append('search', searchTerm);
-//   limit && searchParams.append('limit', limit.toString());
-//   currentPage && searchParams.append('page', currentPage.toString());
-//   return searchParams.toString();
-// };
-
-const PeopleSection = ({
-  children,
-  people,
-}: PropsWithChildren<Props>): JSX.Element => {
+const PeopleSection = ({ children, people }: PropsWithChildren<Props>) => {
   const router = useRouter();
-  //const [loading, setLoading] = useState(false);
-  //const searchTerm = (router.query.search as string) || '';
-  //const limit = Number(router.query.limit) || 10;
-  //const currentPage = Number(router.query.page) || 1;
-
-  const searchParams = new URLSearchParams(encode(router.query));
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -54,9 +31,13 @@ const PeopleSection = ({
       Router.events.off('routeChangeError', handlerOff);
     };
   }, []);
+
   if (loading) return <Loading />;
-  const persons = [...people.results];
-  persons.length = Number(searchParams.get('limit')) || DEFAULT_LIMIT;
+  const searchParams = new URLSearchParams(encode(router.query));
+  const persons = (people?.results || []).slice(
+    0,
+    Number(searchParams.get('limit')) || DEFAULT_LIMIT
+  );
 
   return (
     <section className={styles.section_wrapper}>
@@ -72,7 +53,7 @@ const PeopleSection = ({
             </ul>
             {children}
           </div>
-          <Pagination totalItems={people.count as number} />
+          <Pagination key={router.asPath} totalItems={people?.count} />
         </>
       )}
     </section>
